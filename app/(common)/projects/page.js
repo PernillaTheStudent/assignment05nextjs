b2accdc909f88595c76c6a29426d0d69b9d741ba
PernillaTheStudent <pernilla.widerberg@edu.cmeducations.se>
2023-07-31 22:45:20 +0200

    Add portfolio text

--- app/(common)/projects/page.js
+++ app/(common)/projects/page.js
@@ -1,39 +1,38 @@
+import portoStyle from "./portfolio/page.module.css";
+import Image from "next/image";
+import Link from "next/link";
+import portfolioOne from "./assets/portfolio.png";
+import portfolioTwo from "./assets/portfolio2.png";
 import styles from "./page.module.css";
 
 export default function Projects() {
   return (
     <>
-      <h1>How to add an array in React state</h1>
+      <h1>Next.js 13</h1>
       <p className={styles.ingress}>
-        Nullam quis imperdiet augue. Vestibulum auctor ornare leo, non suscipit
-        magna interdum eu. Curabitur pellentesque nibh nibh, at maximus ante
-        fermentum sit amet. Pellentesque commodo lacus at sodales sodales.
+        This very site you are looking at is the result of the last assignment in the React course (actually JS Ramverk in swedish). The aim was to build a portfolio using Next.js 13 with App folders and Layouts.
       </p>
-      <h2>Creating React state as an array</h2>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi.
-        Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla,
-        mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis
-        tellus. Nullam quis imperdiet augue. Vestibulum auctor ornare leo, non
-        suscipit magna interdum eu. Curabitur pellentesque nibh nibh, at maximus
-        ante fermentum sit amet. Pellentesque commodo lacus at sodales sodales.
-        Quisque sagittis orci ut diam condimentum, vel euismod erat placerat.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi.
-        Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla,
-        mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis
-        tellus.
-      </p>
-      <p>
-        Nullam quis imperdiet augue. Vestibulum auctor ornare leo, non suscipit
-        magna interdum eu. Curabitur pellentesque nibh nibh, at maximus ante
-        fermentum sit amet. Pellentesque commodo lacus at sodales sodales.
-        Quisque sagittis orci ut diam condimentum, vel euismod erat placerat. In
-        iaculis arcu eros, eget tempus orci facilisis id.Lorem ipsum dolor sit
-        amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit
-        urna. Pellentesque sit
-      </p>
+      <h2>Portfolio</h2>
+      <div style={{ width: "100%", display: "flex", padding: "48px 0", margin: "0px 0 40px", alignItems: "center", flexDirection: "column", backgroundColor: "#F9F2E5", justifyContent: "center", }}>
+        <Image
+          className={portoStyle.single}
+          style={{ marginBottom: "22px" }}
+          alt="PW codex portfolio"
+          src={portfolioOne}
+          width={1000}
+          height={650}
+        />
+        <Image
+          className={portoStyle.single}
+          alt="PW codex portfolio"
+          src={portfolioTwo}
+          width={1000}
+          height={650}
+        />
+      </div>
+      <p>The design was created in Figma with patterns (slightly adjusted) and inspiration from Paaatterns. </p>
+      <p>A customized 404 page was included.</p>
+      <Link href="https://products.ls.graphics/paaatterns/preview.html" target="_blank">Paaatterns</Link>
     </>
   );
 }
--- app/(common)/projects/portfolio/page.js
+++ app/(common)/projects/portfolio/page.js
@@ -10,7 +10,7 @@ export default function Portfolio({ params }) {
     <>
       <h1>Next.js 13</h1>
       <p className={styles.ingress}>
-        This very site you are looking at is the result of the assignment – to build a portfolio using Next.js 13 with App folders and Layouts.
+        This very site you are looking at is the result of the last assignment in the React course (actually JS Ramverk in swedish). The aim was to build a portfolio using Next.js 13 with App folders and Layouts.
       </p>
       <h2>Portfolio</h2>
       <div style={{ width: "100%", display: "flex", padding: "48px 0", margin: "0px 0 40px", alignItems: "center", flexDirection: "column", backgroundColor: "#F9F2E5", justifyContent: "center", }}>
