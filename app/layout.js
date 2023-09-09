cb186ca5d4019cb7b7bcd00b795e24e7c4339a1c
PernillaTheStudent <pernilla.widerberg@edu.cmeducations.se>
2023-09-09 13:27:33 +0200

    Add metatags

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Signika_Negative, Montserrat, Lora } from "next/font/google";
+// import type { Metadata } from "next";
 
 const signika_negative = Signika_Negative({
   variable: "--font-signika-negative",
@@ -26,6 +27,23 @@ const lora = Lora({
 export const metadata = {
   title: "PW Portfolio -- code(x)",
   description: "Building useful & beautiful interfaces",
+  generator: 'Next.js',
+  applicationName: 'Next.js',
+  referrer: 'origin-when-cross-origin',
+  keywords: ['Next.js', 'React', 'JavaScript'],
+  // authors: [{ name: 'Seb' }, { name: 'Josh', url: 'https://nextjs.org' }],
+  // colorScheme: 'dark',
+  // creator: 'Jiachi Liu',
+  // publisher: 'Sebastian Markbåge',
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
+  },
+  verification: {
+    google: "0Wqv3AczPZTQBVvCeocQ9NKrcVC4qYqFKE9dhzqWmP8",
+  },
+  viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
 };
 
 export default function RootLayout({ children }) {
@@ -34,6 +52,9 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${lora.variable} ${signika_negative.variable} ${montserrat.variable}`}
     >
+      {/* <Head>
+        <title>PW Portfolio</title>
+      </Head> */}
       <body>{children}</body>
     </html>
   );
